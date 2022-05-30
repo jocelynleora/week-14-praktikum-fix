5008894f8e40fa2553c4f0ae95ea5b9d8c8755f7
Jocelyn Leora <jocelyn.leora@gmail.com>
2022-05-30 01:26:20 -0700

    Add project files.

---
+++ week 14 praktikum-fix/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace week_14_praktikum_fix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView_last5match = new System.Windows.Forms.DataGridView();
+            this.labelWorst = new System.Windows.Forms.Label();
+            this.labelTopScorer = new System.Windows.Forms.Label();
+            this.labelStadium = new System.Windows.Forms.Label();
+            this.labelManager = new System.Windows.Forms.Label();
+            this.labelTeamName = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonNext2 = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrev = new System.Windows.Forms.Button();
+            this.buttonPrev2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_last5match)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_last5match
+            // 
+            this.dataGridView_last5match.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_last5match.Location = new System.Drawing.Point(40, 247);
+            this.dataGridView_last5match.Name = "dataGridView_last5match";
+            this.dataGridView_last5match.Size = new System.Drawing.Size(389, 191);
+            this.dataGridView_last5match.TabIndex = 44;
+            // 
+            // labelWorst
+            // 
+            this.labelWorst.AutoSize = true;
+            this.labelWorst.Location = new System.Drawing.Point(132, 222);
+            this.labelWorst.Name = "labelWorst";
+            this.labelWorst.Size = new System.Drawing.Size(41, 13);
+            this.labelWorst.TabIndex = 43;
+            this.labelWorst.Text = "label10";
+            // 
+            // labelTopScorer
+            // 
+            this.labelTopScorer.AutoSize = true;
+            this.labelTopScorer.Location = new System.Drawing.Point(132, 193);
+            this.labelTopScorer.Name = "labelTopScorer";
+            this.labelTopScorer.Size = new System.Drawing.Size(35, 13);
+            this.labelTopScorer.TabIndex = 42;
+            this.labelTopScorer.Text = "label9";
+            // 
+            // labelStadium
+            // 
+            this.labelStadium.AutoSize = true;
+            this.labelStadium.Location = new System.Drawing.Point(132, 159);
+            this.labelStadium.Name = "labelStadium";
+            this.labelStadium.Size = new System.Drawing.Size(35, 13);
+            this.labelStadium.TabIndex = 41;
+            this.labelStadium.Text = "label8";
+            // 
+            // labelManager
+            // 
+            this.labelManager.AutoSize = true;
+            this.labelManager.Location = new System.Drawing.Point(132, 128);
+            this.labelManager.Name = "labelManager";
+            this.labelManager.Size = new System.Drawing.Size(35, 13);
+            this.labelManager.TabIndex = 40;
+            this.labelManager.Text = "label7";
+            // 
+            // labelTeamName
+            // 
+            this.labelTeamName.AutoSize = true;
+            this.labelTeamName.Location = new System.Drawing.Point(132, 98);
+            this.labelTeamName.Name = "labelTeamName";
+            this.labelTeamName.Size = new System.Drawing.Size(35, 13);
+            this.labelTeamName.TabIndex = 39;
+            this.labelTeamName.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Worst Discipline :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Top Scorer :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Stadium :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Manager :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Team Name :";
+            // 
+            // buttonNext2
+            // 
+            this.buttonNext2.Location = new System.Drawing.Point(239, 31);
+            this.buttonNext2.Name = "buttonNext2";
+            this.buttonNext2.Size = new System.Drawing.Size(67, 44);
+            this.buttonNext2.TabIndex = 33;
+            this.buttonNext2.Text = ">>";
+            this.buttonNext2.UseVisualStyleBackColor = true;
+            this.buttonNext2.Click += new System.EventHandler(this.buttonNext2_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(173, 31);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(67, 44);
+            this.buttonNext.TabIndex = 32;
+            this.buttonNext.Text = ">";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrev
+            // 
+            this.buttonPrev.Location = new System.Drawing.Point(107, 31);
+            this.buttonPrev.Name = "buttonPrev";
+            this.buttonPrev.Size = new System.Drawing.Size(67, 44);
+            this.buttonPrev.TabIndex = 31;
+            this.buttonPrev.Text = "<";
+            this.buttonPrev.UseVisualStyleBackColor = true;
+            this.buttonPrev.Click += new System.EventHandler(this.buttonPrev_Click);
+            // 
+            // buttonPrev2
+            // 
+            this.buttonPrev2.Location = new System.Drawing.Point(41, 31);
+            this.buttonPrev2.Name = "buttonPrev2";
+            this.buttonPrev2.Size = new System.Drawing.Size(67, 44);
+            this.buttonPrev2.TabIndex = 30;
+            this.buttonPrev2.Text = "<<";
+            this.buttonPrev2.UseVisualStyleBackColor = true;
+            this.buttonPrev2.Click += new System.EventHandler(this.buttonPrev2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_last5match);
+            this.Controls.Add(this.labelWorst);
+            this.Controls.Add(this.labelTopScorer);
+            this.Controls.Add(this.labelStadium);
+            this.Controls.Add(this.labelManager);
+            this.Controls.Add(this.labelTeamName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonNext2);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonPrev);
+            this.Controls.Add(this.buttonPrev2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_last5match)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_last5match;
+        private System.Windows.Forms.Label labelWorst;
+        private System.Windows.Forms.Label labelTopScorer;
+        private System.Windows.Forms.Label labelStadium;
+        private System.Windows.Forms.Label labelManager;
+        private System.Windows.Forms.Label labelTeamName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonNext2;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrev;
+        private System.Windows.Forms.Button buttonPrev2;
+    }
+}
+
---
+++ week 14 praktikum-fix/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace week_14_praktikum_fix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection sqlConnect = new MySqlConnection("server=localhost;uid=root;pwd=;database=premier_league");
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        String sqlQuery;
+        DataTable dtData = new DataTable();
+        DataTable dtScore = new DataTable();
+        DataTable dtGrid = new DataTable();
+        DataTable dtWorst = new DataTable();
+        int posisiSekarang = 0;
+
+        public void IsiDataPemain(int Posisi)
+        {
+            labelTeamName.Text = dtData.Rows[Posisi][0].ToString();
+            labelManager.Text = dtData.Rows[Posisi][1].ToString();
+            labelStadium.Text = dtData.Rows[Posisi][2].ToString();
+            string save = dtData.Rows[Posisi][3].ToString();
+            dtGrid = new DataTable();
+            dtWorst = new DataTable();
+            posisiSekarang = Posisi;
+
+            sqlQuery = "select concat(p.player_name, ' ', sum(dm.type = 'GO' OR dm.type = 'GP'), '(', sum(dm.type = 'GP'),')') from dmatch dm, player p where p.team_id = '" + dtData.Rows[Posisi]["team_id"] + "' and (dm.type = 'GO' OR dm.type = 'GP') and dm.player_id = p.player_id group by dm.player_id order by 1 desc;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtScore);
+            labelTopScorer.Text = dtScore.Rows[Posisi][0].ToString();
+
+            sqlQuery = "SELECT date_format(m.match_date, '%d/%c/%Y') as'match_date' , if(m.team_home = '" + save + "','HOME',if(team_away = '" + save + "','AWAY',0)) as 'Home/Away',if(m.team_home = '" + save + "',m.team_away,if(m.team_away = '" + save + "',m.team_home,0)) as 'lawan' ,concat(m.goal_home,'-',m.goal_away) as 'score' FROM `match` m where m.team_home = '" + save + "' or m.team_away = '" + save + "' order by m.match_date desc LIMIT 5 ;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtGrid);
+            dataGridView_last5match.DataSource = dtGrid;
+
+            sqlQuery = "SELECT p.player_name, SUM(if(d.`type`='CR',1,0)) , SUM(if(d.`type` = 'CY', 1, 0)) ,  sum(if(d.type = 'CY',1,0)) + sum(if(d.type = 'CR',3,0)) as 'poin'  from player p, dmatch d, team t where p.player_id = d.player_id and t.team_id = p.team_id and t.team_id='" + save + "' group by p.player_id order by poin desc;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtWorst);
+
+            labelWorst.Text = dtWorst.Rows[0][0].ToString() + " ," + dtWorst.Rows[0][2].ToString() + " Yellow Card and " + dtWorst.Rows[0][1].ToString() + " Red Card";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlQuery = "select t.team_name, concat(m.manager_name, ' (' ,n.nation, ')'), concat(t.home_stadium, ', ',  t.city, ' (',t.capacity,')') , team_id from team t, manager m, nationality n where t.manager_id = m.manager_id and n.nationality_id = m.nationality_id; ";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtData);
+
+            DataTable dtMatch = new DataTable();
+            sqlQuery = "select match_date from `match`";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtMatch);
+            dataGridView_last5match.DataSource = dtMatch;
+
+            IsiDataPemain(0);
+        }
+
+        private void buttonPrev2_Click(object sender, EventArgs e)
+        {
+            IsiDataPemain(0);
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            if (posisiSekarang > 0)
+            {
+                posisiSekarang--;
+                IsiDataPemain(posisiSekarang);
+            }
+            else
+            {
+                MessageBox.Show("Data Sudah Data Pertama");
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            if (posisiSekarang < dtData.Rows.Count - 1)
+            {
+                posisiSekarang++;
+                IsiDataPemain(posisiSekarang);
+            }
+            else
+            {
+                MessageBox.Show("Data Sudah Data Terakhir");
+            }
+        }
+
+        private void buttonNext2_Click(object sender, EventArgs e)
+        {
+            IsiDataPemain(dtData.Rows.Count - 1);
+        }
+    }
+}
